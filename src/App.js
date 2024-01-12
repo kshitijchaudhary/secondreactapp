a0cd04641f1d6eada41350cb0058309b6b2f3756
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-01-12 17:18:32 -0500

    Props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-    {/* <Greet />
-    <Welcome /> */}
-    <Hello />
+    <Greet name = "Bruce" heroName = "Batman">
+    <p>This is Children Props</p></Greet>
+    <Greet name ="Clark" heroName = "Superman">
+      <button>Click Me</button>
+    </Greet>
+    <Greet name=  "Diana" heroName = "Boxman" />
+    <Welcome  name = "Bruce" heroName = "Batman" />
+    <Welcome name ="Clark" heroName = "Superman" />
+    <Welcome  name=  "Diana" heroName = "Boxman" />
+    {/* <Hello /> */} 
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,6 +4,13 @@ import React from 'react'
 //     return <h1>Hello Xitol</h1>
 // }
 
-const Greet= () => <h1>Hello Xitol</h1> 
-
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+        <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+        {props.children}
+        </div>
+    ) 
+}
 export default Greet
