56b00b2ab993ef0829ad4101c25180f66589c238
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-01-13 20:05:05 -0500

    Binding Event handlers with four approaches

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import { Component } from 'react';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 class App extends Component {
 
   render(){
   return (
     <div className="App">
-    <FunctionClick />
-    <ClassClick />
+    <EventBind />
+    {/* <FunctionClick />
+    <ClassClick /> */}
     {/* <Counter /> */}
     {/* <Message /> */}
     {/* <Greet name = "Bruce" heroName = "Batman">
