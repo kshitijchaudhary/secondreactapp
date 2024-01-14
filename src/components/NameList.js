31992d4b593235ae7abe5bfb7032486162442716
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-01-14 16:26:18 -0500

    Index as Key and its patterns

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -3,6 +3,7 @@ import Person from './Person'
 
 // list rendering
 function NameList() {
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
     const persons = [
         {
             id: 1,
@@ -23,8 +24,9 @@ function NameList() {
             skill: 'Vue'
         }
     ]
-    const personList = persons.map(person => <Person key= {person.name} person = {person} />)
-    return (<div> {personList} </div>)
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+    return (<div> {nameList} </div>)
 }
+
 
 export default NameList
