6241e90fb713445d7d159ad40e9e55c8df4faab3
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-01-15 01:37:42 -0500

    Form Handling in React

--- src/App.js
+++ src/App.js
@@ -16,15 +16,19 @@ import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css'
+import Form from './components/Form.js'
 
 class App extends Component {
 
   render(){
   return (
     <div className="App">
-      <h1 className='error'>Error</h1>
+     <Form />
+     
+     
+      {/* <h1 className='error'>Error</h1>
       <h1 className={styles.success}>Success</h1>
-      <Inline />
+      <Inline /> */}
     {/* <Stylesheet primary={true} />   */}
     {/* <NameList /> */}
    {/* <UserGreeting /> */}
