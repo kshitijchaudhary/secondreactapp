8dceffb7f1b15031606be81980d19915728aa573
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-01-15 01:06:47 -0500

    Styling in CSS Basic

--- src/App.js
+++ src/App.js
@@ -12,13 +12,21 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 class App extends Component {
 
   render(){
   return (
     <div className="App">
-      <NameList />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline />
+    {/* <Stylesheet primary={true} />   */}
+    {/* <NameList /> */}
    {/* <UserGreeting /> */}
     {/* <ParentComponent /> */}
     {/* <EventBind /> */}
